62f118e3a683616173473eeb803fc051f3a712bb
Leonardo <lvneves2010@gmail.com>
2020-05-02 15:57:29 -0300

    CRUD Ok

---
+++ src/server.js
@@ -0,0 +1,43 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+const bancoDeDados = require( './bancoDeDados' )
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get( '/produtos', ( req, res, next ) => {
+    res.send( bancoDeDados.getProdutos() )
+} )
+
+app.get( '/produtos/:id', ( req, res ) => {
+    res.send(bancoDeDados.getProduto( req.params.id ))
+} )
+
+app.post( '/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+} )
+
+app.put( '/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+} )
+
+app.delete( '/produtos/:id', (req, res, next) => {
+    const produto = bancoDeDados.excluirProduto(req.params.id)
+    res.send(produto)
+} )
+
+app.listen(port, () => {
+    console.log( `Servidor na porta ${port}` )
+} )
